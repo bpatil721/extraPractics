eb5d71888148573f54190287a9f9d17aab6ca4c7
Bhushan <bhushanp721@gmail.com>
2026-05-20 08:42:42 +0530

    test

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -77,6 +77,7 @@ $(document).ready(function(){
     }
 
     $(document).on('submit','.ajax-form',function(e){
+        +
         e.preventDefault();
         let form = $(this);
         // let url = form.attr('action');
